f010eca56be3e11ffcd758d2c85743bf7b3d561c
Vilamonjo <vilamonjo@gmail.com>
2018-06-11 20:05:41 +0200

    v0.1 Array of structs, load from file and save

---
+++ POS-Garage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public struct Customer
+{
+    string name;
+    string cif;
+    string residence;
+    string city;
+    ushort postalCode;
+    string country;
+    uint phoneNumber;
+    string eMail;
+    string contact;
+    string observations;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer[] allCustomers = new Customer[1000];
+        Program.DisplayMenu();
+        
+    }
+
+    public static void DisplayMenu()
+    {
+        Console.SetCursorPosition(0, Console.WindowHeight - 3);
+        Console.ReadLine();
+    }
+}
+
